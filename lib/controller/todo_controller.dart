e268b45ea13c883138eae12afa98b8c211b63c50
ihsuncloud <ihsun@tuta.io>
2024-09-24 03:25:37 +0330

    🚀 Add todo controller.

---
+++ lib/controller/todo_controller.dart
@@ -0,0 +1,20 @@
+import 'package:get/get.dart';
+import '../models/todo.dart';
+
+class TodoController extends GetxController {
+  var todos = <Todo>[].obs;
+
+  void addTodo(String title) {
+    todos.add(Todo(title: title));
+  }
+
+  void toggleTodoStatus(int index) {
+    var todo = todos[index];
+    todo.isDone = !todo.isDone;
+    todos[index] = todo;
+  }
+
+  void deleteTodo(int index) {
+    todos.removeAt(index);
+  }
+}
